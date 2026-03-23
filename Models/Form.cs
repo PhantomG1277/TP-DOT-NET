c189fcda06b8804b471b4e4d6c52eb87d8fe8b1a
Etienne Porquier <erefeq@outlook.fr>
2026-03-23 18:50:41 +0100

    Missing Validation, may have to rewrite code

---
+++ Models/Form.cs
@@ -0,0 +1,45 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TPLOCAL1.Models
+{
+    public class Form
+    {
+        [Required]
+        public string? Nom { get; set; }
+
+        [Required]
+        public string? Prenom { get; set; }
+
+        [Required]
+        public string? Genre { get; set;}
+
+        [Required]
+        [StringLength(100, MinimumLength = 10)]
+        public string? Adresse {  get; set;}
+
+        [Required]
+        [RegularExpression(@"^\d{5}$")]
+        public int? CodePostal { get; set; }
+        
+        [Required]
+        public string? Ville { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string? AdresseCourriel { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+
+        public DateTime? DateDebut { get; set; }
+
+        [Required]
+        public string? Formation { get; set; }
+
+        [Required]
+        public string? AvisMainframe { get; set; }
+
+        [Required]
+        public string? AvisCs {  get; set; }
+    }
+}
